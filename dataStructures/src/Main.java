c84ce80e28a9a327cd3ef11cc22cd60e64985880
Aidan James <79814378+aidanjames97@users.noreply.github.com>
2024-01-11 17:49:59 -0500

    Add files via upload

---
+++ dataStructures/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    private int[] nodes = {1,3,5,7,9,2,4,6,8}; // 9 ints used for node data
+    public static void main(String[] args) {
+        System.out.println("this is for testing my knowledge of data structures");
+    }
+
+    // getter for nodes info used from other classes
+    public int[] getNodes() {
+        return nodes;
+    }
+}
